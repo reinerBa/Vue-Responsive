d12487ecfebcaf61c5d91bbf7b28c49bfb232606
reinerBa <reinerbam@mailbox.org>
2017-12-12 00:08:55 +0100

    Fixed a getter only function

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-﻿var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require("gulp-rename");
-var replace = require('gulp-replace');
+﻿var gulp = require('gulp')
+var uglify = require('gulp-uglify')
+var standard = require('gulp-standard')
+var rename = require("gulp-rename")
+var replace = require('gulp-replace')
 var fs = require('fs')
 var version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version
 
 gulp.task('default',function(){
 	gulp.src('./Vue-Responsive.js')
+	    .pipe(standard())
+        .pipe(standard.reporter('default', {
+           breakOnError: true,
+           quiet: true
+        }))
 		.pipe(replace(/(@@version@@)+/, version))
 		.pipe(gulp.dest('./dist/'))
 		.pipe(uglify({preserveComments: 'license'}))
