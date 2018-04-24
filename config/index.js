17af21922e750e5854adbb4f72948ded98ef12bd
reinerBa <reinerbam@mailbox.org>
2018-04-24 14:28:44 +0200

    Make spa demo fit

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static/',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
