40e9d140253c4eebb7ac6efe2cb2c9b13f62caf6
reinerBa <4329883+reinerBa@users.noreply.github.com>
2022-01-19 22:54:16 +0100

    Add Typescript support

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+export default vueResponsive;
+export namespace vueResponsive {
+    /**
+     * Install vue-responsive globally to Vue.js
+     *
+     * @param  {object} Vue     the constructor of the framework
+     * @param  {object} options parameter to modify the behavior of the plugin
+     * @return {void}         returns nothing
+     */
+    function install(Vue: any, options: any): void;
+}
+//# sourceMappingURL=index.d.ts.map
